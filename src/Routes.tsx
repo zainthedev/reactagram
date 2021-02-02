f850fed11f750e18bac2c4ab7ad08900e6c59c59
Zain Hill <zainthedev@gmail.com>
2021-02-02 21:29:49 +0900

    Changed: Removed boilerplate

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,13 +1,12 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { App } from "./App";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { App } from './App';
 
 export const Routes = () => {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route exact path="/" component={App} />
-            </Switch>
-        </BrowserRouter>
-    );
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route exact path='/' component={App} />
+			</Switch>
+		</BrowserRouter>
+	);
 };
