84ddee0e6c444eb7ea3c6a6818be810287f16b71
Zain Hill <zainthedev@gmail.com>
2021-02-06 15:39:00 +0900

    Added: AuthCheck hook to router

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
-import { useAuth } from 'reactfire';
+import { AuthCheck } from 'reactfire';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { HomeComponent } from './components/HomeComponent';
 import { LoginComponent } from './components/LoginComponent';
 
 export const App = () => {
-	const auth = useAuth();
-
 	return (
 		<BrowserRouter>
-			{auth.currentUser === null ? (
-				<LoginComponent />
-			) : (
+			<AuthCheck fallback={<LoginComponent />}>
 				<Switch>
 					<Route exact path='/' component={HomeComponent} />
 				</Switch>
-			)}
+			</AuthCheck>
 		</BrowserRouter>
 	);
 };
