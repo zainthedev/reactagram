75d57ae3aed4eaa9ddc81340902fd2a4cf55bd8c
Zain Hill <zainthedev@gmail.com>
2021-02-06 13:19:09 +0900

    Removed: Signupuser props

--- src/components/LoginComponent.tsx
+++ src/components/LoginComponent.tsx
@@ -1,15 +1,13 @@
 import { LoginFormComponent } from './LoginFormComponent';
 import { useState } from 'react';
 import { AuthFormComponent } from './AuthFormComponent';
-import { Login } from '../styled-components/authStyles';
+import { AuthForm, Login } from '../styled-components/authStyles';
 
 export const LoginComponent = () => {
-	const [signupUser, setSignupUser] = useState({ email: '', password: '' });
-
 	return (
 		<Login>
-			<AuthFormComponent signupUser={signupUser}>
-				<LoginFormComponent signupUser={signupUser} />
+			<AuthFormComponent>
+				<LoginFormComponent />
 			</AuthFormComponent>
 		</Login>
 	);
