7473866ba24ef1fae48b735ff57166267bd5e165
Zain Hill <zainthedev@gmail.com>
2021-02-07 00:17:50 +0900

    Added: Username availabilty checks

--- src/components/SignupFormComponent.tsx
+++ src/components/SignupFormComponent.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useAuth, useUser } from 'reactfire';
+import { useAuth, useFirestore, useFirestoreCollectionData } from 'reactfire';
 import { AuthButton } from '../styled-components/authStyles';
 import { FormInputWrapper, FormInput } from '../styled-components/globalStyles';
 import { getInputError } from '../helper-functions/getInputError';
@@ -11,6 +11,22 @@ export const SignupFormComponent = () => {
 	const [signUpError, setSignUpError] = useState({ error: false, message: '' });
 
 	const auth = useAuth();
+
+	const usernameCollectionQuery = useFirestore().collection('usernames');
+	const usernameCollection = useFirestoreCollectionData(usernameCollectionQuery).data;
+
+	const usernames: string[] = [];
+	const populateUsernames = async () => {
+		usernameCollection.forEach((obj: any) => {
+			if (!usernames.includes(username)) {
+				usernames.push(obj.name);
+			}
+		});
+	};
+
+	const addUsername = async () => {
+		await usernameCollectionQuery.add({ name: username.toString() });
+	};
 
 	const handleInput = (e: React.FormEvent<HTMLInputElement>) => {
 		const formValue = e.currentTarget.value;
@@ -28,11 +44,23 @@ export const SignupFormComponent = () => {
 	};
 
 	const signUp = async (email: string, password: string) => {
-		try {
-			await auth.createUserWithEmailAndPassword(email, password);
-			await auth.currentUser?.updateProfile({ displayName: username });
-		} catch (err) {
-			setSignUpError({ error: true, message: getInputError(err) });
+		populateUsernames();
+		if (username.length > 0 && !usernames.includes(username)) {
+			try {
+				await auth.createUserWithEmailAndPassword(email, password);
+				await auth.currentUser?.updateProfile({ displayName: username });
+				addUsername();
+				console.log(usernames);
+			} catch (err) {
+				console.log(err);
+				setSignUpError({ error: true, message: getInputError(err) });
+			}
+		} else {
+			console.log(usernames);
+			setSignUpError({
+				error: true,
+				message: username.length === 0 ? 'Enter username' : 'Username unavailable',
+			});
 		}
 	};
 
--- src/helper-functions/getInputError.ts
+++ src/helper-functions/getInputError.ts
@@ -1,6 +1,6 @@
 export const getInputError = (err: any) => {
 	let error = '';
-	if (err.code === 'auth/invalid-email') {
+	if (err.code === 'auth/invalid-email' || err.code === 'auth/email-already-in-use') {
 		error = 'Invailid email address.';
 	} else if (err.code === 'auth/wrong-password' || err.code === 'auth/user-not-found') {
 		error = 'The password or username you entered is incorrect.';
