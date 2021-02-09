eb5e00fba3bef86b7bd82aead623a56348d55a3b
Zain Hill <zainthedev@gmail.com>
2021-02-09 16:39:39 +0900

    Changed: Refactored UserProfile to fetch correct data

--- src/components/UserProfileComponent.tsx
+++ src/components/UserProfileComponent.tsx
@@ -1,45 +1,31 @@
-import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
-	Link,
-	Redirect,
-	useParams,
-	useRouteMatch,
-} from 'react-router-dom';
-import {
-	useUser,
-	useAuth,
-	useFirestore,
-	useFirestoreCollection,
-	useFirestoreCollectionData,
-	useFirestoreDoc,
-} from 'reactfire';
+import { useParams } from 'react-router-dom';
+import { useFirestore, useFirestoreCollectionData } from 'reactfire';
 import { useEffect, useState } from 'react';
 import { UserProfile } from '../styled-components/profileStyles';
 
 export const UserProfileComponent = () => {
-	const [username, setUsername] = useState('');
+	const [user, setUser] = useState({
+		name: '',
+		followers: [],
+		following: [],
+		likes: [],
+		displayPicture: '',
+	});
 
-	const user = useUser();
-	const userImage = user.data.photoURL;
+	const { profile }: any = useParams();
 
-	const auth = useAuth();
 	const userCollectionQuery = useFirestore().collection('users');
 	const userCollectionData = useFirestoreCollectionData(userCollectionQuery);
 
 	useEffect(() => {
 		if (userCollectionData.data !== undefined) {
-			const foundUser: any = userCollectionData.data.find((p) => p.name === user.data.displayName);
-			const foundUsername = foundUser.name;
-			setUsername(foundUsername);
-			console.log(username);
+			const foundUser: any = userCollectionData.data.find((p) => p.name === profile);
+			if (foundUser !== undefined) {
+				const foundUsername = foundUser;
+				setUser(foundUsername);
+			}
 		}
-	}, [userCollectionData.data, user.data.displayName, username]);
-	let { url } = useRouteMatch();
-	let { profile }: any = useParams();
-	console.log(url);
-	console.log(profile);
+	}, [userCollectionData.data, profile]);
 
-	return <UserProfile>{profile}'s profile</UserProfile>;
+	return <UserProfile>{user.name}'s profile</UserProfile>;
 };
--- src/styled-components/homeStyles.ts
+++ src/styled-components/homeStyles.ts
@@ -2,4 +2,5 @@ import styled from 'styled-components';
 
 export const Home = styled.div`
 	display: flex;
+	flex-direction: column;
 `;
