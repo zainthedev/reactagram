b4b98789e0e8f2094ea30840eb413e9470473d4c
Zain Hill <zainthedev@gmail.com>
2021-02-09 21:48:03 +0900

    Added: Built on UserProfileComponent

--- src/components/UserProfileInfoComponent.tsx
+++ src/components/UserProfileInfoComponent.tsx
@@ -1,14 +1,28 @@
 import { Link } from 'react-router-dom';
-import { ImageWrapper, UserIcon } from '../styled-components/imageStyles';
-import { UserProfileInfo } from '../styled-components/profileStyles';
+import { UserProfileImageWrapper, UserIcon } from '../styled-components/imageStyles';
+import {
+	UserProfileInfoWrapper,
+	UserProfileInfo,
+	UserProfileName,
+	UserProfileStats,
+	UserProfileStat,
+} from '../styled-components/profileStyles';
 import { UserType } from '../types';
 
 export const UserProfileInfoComponent = ({ user }: UserType) => {
 	return (
-		<UserProfileInfo>
-			<ImageWrapper>
+		<UserProfileInfoWrapper>
+			<UserProfileImageWrapper>
 				<UserIcon src={user.displayPicture} />
-			</ImageWrapper>
-		</UserProfileInfo>
+			</UserProfileImageWrapper>
+			<UserProfileInfo>
+				<UserProfileName>{user.name}</UserProfileName>
+				<UserProfileStats>
+					<UserProfileStat>{user.posts.length} posts</UserProfileStat>
+					<UserProfileStat>{user.followers.length} followers</UserProfileStat>
+					<UserProfileStat>{user.following.length} following</UserProfileStat>
+				</UserProfileStats>
+			</UserProfileInfo>
+		</UserProfileInfoWrapper>
 	);
 };
