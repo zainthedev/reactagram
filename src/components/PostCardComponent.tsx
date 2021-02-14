ff7ec36447d4754ffb70713c8d152c4cd4768a6f
Zain Hill <zainthedev@gmail.com>
2021-02-14 12:44:13 +0900

    Added: PostCardComponent

---
+++ src/components/PostCardComponent.tsx
@@ -0,0 +1,6 @@
+import { PostCard } from '../styled-components/postStyles';
+import { PostType } from '../types';
+
+export const PostCardComponent = ({ caption, comments, image, location, poster }: PostType) => {
+	return <PostCard>Home</PostCard>;
+};
